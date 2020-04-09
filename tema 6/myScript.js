7a131b1176be693b88879cbdcba392f5b178e229
Ioana-Petrea <63294313+Ioana-Petrea@users.noreply.github.com>
2020-04-09 13:03:37 +0300

    Create myScript.js

---
+++ tema 6/myScript.js
@@ -0,0 +1,5 @@
+function schimba_culori(){
+				document.getElementById("titlu").style.color="forestgreen"
+				document.getElementById("titlu_filme").style.color="green"
+				document.GetElementById("titlu_seriale").style.color="seagreen"
+}
