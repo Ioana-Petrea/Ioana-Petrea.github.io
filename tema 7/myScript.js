1d414c8e1ccdbcc780d288a1810e2b88605904f5
Ioana-Petrea <63294313+Ioana-Petrea@users.noreply.github.com>
2020-04-09 13:08:10 +0300

    Create myScript.js

---
+++ tema 7/myScript.js
@@ -0,0 +1,13 @@
+function change_colors() {
+	document.getElementById("titlu").style.color = " #800000";
+	document.getElementById("titlu_filme").style.color = "yellow";
+	document.getElementById("titlu_seriale").style.color = "blue";
+}
+
+function askForName() {
+	nume = prompt("Introduceti numele:")
+	document.getElementById("nume_user").innerHTML = nume + " " + "tea rugat iulia sai dai si ei osuta dojda mi sa sia tigari si maine ti da" + "!"
+}
+function arata_alert() {
+	window.alert("CAVALERII TUNULUI INCLINAT nu promoveaza fumatul, dar daca tot o faci sa fie pe cheltuiala ta ! 😉");
+}
