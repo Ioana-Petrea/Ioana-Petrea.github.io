cb726cc59f0c4508ef061498289f0c4710104f00
Ioana-Petrea <63294313+Ioana-Petrea@users.noreply.github.com>
2020-04-15 13:07:32 +0300

    Create myscript.js

---
+++ tema10/myscript.js
@@ -0,0 +1,9 @@
+function ask_for_movie(){
+	film=prompt("Care este filmul tau favorit? ")
+    document.getElementById("mygroup").innerHTML ="Introdu " + "filmul " + "care " + "iti " + "place" + "cel" + "mai" + "mult" 
+    }
+function arata_alert(){
+window.alert( Salutare! )
+
+function arataAlert(){
+window.alert ("FELICITARI, AI PARCURS TOATA PAGINA 🎇")
