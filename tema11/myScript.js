eddc8975facc4bb8fab9c45967d3ec04cf62fe44
Ioana-Petrea <63294313+Ioana-Petrea@users.noreply.github.com>
2020-04-15 23:50:09 +0300

    Create myScript.js

---
+++ tema11/myScript.js
@@ -0,0 +1,6 @@
+function ask_for_movie(){
+	film=prompt("Care este filmul tau favorit? ")
+    document.getElementById("mygroup").innerHTML ="Introdu " + "filmul " + "care " + "iti " + "place" + "cel" + "mai" + "mult" 
+    }
+function arata_alert(){
+window.alert( Salutare! )
